331616252e76711d079b51d8b39af5c6926fa2b8
Shu Isaka@Lab <shu.h7.research@gmail.com>
2018-07-05 15:41:58 +0900

    Correction for the comment

--- project/project/test/mymodule_test.py
+++ project/project/test/mymodule_test.py
@@ -4,17 +4,15 @@ sys.path.append("../src")
 import unittest
 import mymodule
 
-class TestCalcSum(unittest.TestCase):
-    """test class of tashizan.py
+class TestMymodule(unittest.TestCase):
+    """test class of mymodule.py
     """
 
     def test_calc_sum(self):
         """test method for calc_sum
         """
-        value1 = 2
-        value2 = 6
         expected = 8
-        actual = mymodule.calc_sum(value1, value2)
+        actual = mymodule.calc_sum(2, 6)
         self.assertEqual(expected, actual)
 
 if __name__ == "__main__":
