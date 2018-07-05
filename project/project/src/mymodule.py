fbe3ced892ca5e1875a9caa31f5fb138ab9acd7f
Shu Isaka@Lab <shu.h7.research@gmail.com>
2018-07-05 16:39:51 +0900

    Added class to mymodule and created test for the added class.

--- project/project/src/mymodule.py
+++ project/project/src/mymodule.py
@@ -11,20 +11,95 @@ Description:
     en - Example module that is called from the other files.
 """
 
+import math
+
 def calc_sum(x: int, y: str) -> int:
-    """足し算． / Summamtion.
+    """足し算 / Summamtion.
 
     xとyを足して返す.
     Returns summation of x and y.
 
     Args:
-        x: １つ目の引数 / arg 1
-        y: ２つ目の引数 / arg 2
+        x (int): １つ目の引数 / arg 1
+        y (int): ２つ目の引数 / arg 2
 
     Returns:
         xとyの合計値．
         Summation of x and y.
-
     """
     return (x + y)
 
+class QudraticEquations(object):
+    """２次方程式のクラス / Class for Quadratic Equations
+    
+    Args:
+        object (object): object
+    """
+
+    def __init__(self, a: float, b: float, c: float) -> None:
+        """コンストラクタ / Constructor
+
+        以下の２次関数のための初期化関数．
+        Initialize the quadratic equations for the equations below.
+            a*x^2 + b*x + c
+        
+        Args:
+            a (float): x^2の係数 / Coefficient for x^2
+            b (float): xの係数 / Coefficient for x
+            c (float): 定数 / Constant Value
+        
+        Returns:
+            None: Returns nothing.
+        """
+        self.a = a
+        self.b = b
+        self.c = c
+    
+    def calc_root(self) -> str:
+        """２次方程式の根を求める / Calculate Root.
+        
+        Returns:
+            str: 根 / root
+        """
+        root = ""
+        
+        D = self._calc_D()
+        if D > 0:
+            # 判別式Dが正の時 / Where D > 0
+            ans1 = ((-1 * self.b) + math.sqrt(D)) / (2 * self.a)
+            ans2 = ((-1 * self.b) - math.sqrt(D)) / (2 * self.a)
+            root += str(ans1) + ", "
+            root += str(ans2)
+        elif D == 0:
+            # 判別式Dが0の時 / Where D = 0
+            ans = ((-1 * self.b)) / (2 * self.a)
+            root += str(ans)
+        else:
+            # 判別式Dが負の時 / Where D < 0
+            real = ((-1 * self.b)) / (2 * self.a)
+            imaginary = math.sqrt(-1 * D) / (2 * self.a)
+            root += str(real) + " + i(" + str(imaginary) + "), "
+            root += str(real) + " - i(" + str(imaginary) + ")"
+
+        return root
+
+    def calc_value(self, x: float) -> float:
+        """f(x)の値を計算する / Calculate f(x).
+        
+        Args:
+            x (float): 二次方程式にダウンロード / value for quadratic function
+        
+        Returns:
+            float: 計算したf(x) / calculated value f(x)
+        """
+        value = self.a * x * x + self.b * x + self.c
+        return value
+    
+    def _calc_D(self) -> float:
+        """判別式の計算 / Calculate Discriminant
+        
+        Returns:
+            float: 判別式の値 / Discriminant Value
+        """
+        D = (self.b * self.b) - 4 * self.a * self.c
+        return D
--- project/project/test/mymodule_test.py
+++ project/project/test/mymodule_test.py
@@ -7,13 +7,37 @@ import mymodule
 class TestMymodule(unittest.TestCase):
     """test class of mymodule.py
     """
+    
+    def setUp(self):
+        print("setup")
+        self.eq1 = mymodule.QudraticEquations(1.0, 3.0, 2.0)
+        self.eq2 = mymodule.QudraticEquations(1.0, 2.0, 1.0)
+        self.eq3 = mymodule.QudraticEquations(1.0, 2.0, 5.0)
 
     def test_calc_sum(self):
         """test method for calc_sum
         """
-        expected = 8
-        actual = mymodule.calc_sum(2, 6)
-        self.assertEqual(expected, actual)
+        self.assertEqual(8, mymodule.calc_sum(2, 6))
+    
+    def test_calc_root(self):
+        """test method for calc_root
+        """
+        self.assertEqual("-1.0, -2.0", self.eq1.calc_root())
+        self.assertEqual("-1.0", self.eq2.calc_root())
+        self.assertEqual("-1.0 + i(2.0), -1.0 - i(2.0)", self.eq3.calc_root())
+        
+    def test_calc_value(self):
+        """test method for calc_value
+        """
+        self.assertEqual(0.0, self.eq1.calc_value(-1))
+        self.assertEqual(0.0, self.eq2.calc_value(-1))
+        self.assertEqual(4.0, self.eq3.calc_value(-1))
+
+    def tearDown(self):
+        print("tearDown")
+        del self.eq1
+        del self.eq2
+        del self.eq3
 
 if __name__ == "__main__":
     unittest.main()
